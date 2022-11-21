6509ba9beeeff45880b6250563a08b373a87a4c4
Marcio Bueno <mabuen@live.com>
2022-11-21 11:39:34 +0100

    aula 30

---
+++ layout.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>aula 30 - layout</title>
+    <link rel="stylesheet" href="layout.css">
+</head>
+<body>
+    <div class="topo"></div>
+    <div class="not"></div>
+    <div class="dest"></div>
+    <div class="preta"></div>
+    <div class="laranja"></div>
+    <div class="amarela"></div>
+    <div class="azul"></div>
+</body>
+</html>
