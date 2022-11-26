52614ca504610eff93f40c249d5a3830d1e9b8c3
mbpfilho <mabuen@live.com>
2022-11-27 00:01:32 +0100

    aula 31 v2

---
+++ aula31/layout2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>aula 31 - layout v2</title>
+    <link rel="stylesheet" href="layout2.css">
+</head>
+<body>
+    <header>Header</header>
+    <nav>Navigation</nav>
+    <main>
+        <div id="part1">Content</div>
+        <div id="part2"></div>
+        <div id="part3"></div>
+    </main>
+        <aside>Sidebar</aside>
+    <footer>Footer</footer>
+</body>
+</html>
