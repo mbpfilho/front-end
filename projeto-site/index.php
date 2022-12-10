5e3d75870ae14b5c685a5344566fe5743540b2f2
Marcio Bueno <mabuen@live.com>
2022-12-10 18:21:12 +0100

    primeira atividade

---
+++ projeto-site/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MB Soluções</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <div class="topo">
+        <div class="logo">
+            <p>MB<br>Soluções</p>
+        </div>
+        <div class="login">
+            <form action="" class="formulario">
+                <input type="email" class="email" placeholder="seu e-mail">
+                <input type="password" name="" id="" class="senha" placeholder="sua senha">
+                <input type="submit" value="Logar" class="botao">
+            </form>
+        </div>
+    </div>
+</body>
+</html>
