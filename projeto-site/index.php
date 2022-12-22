44d3ca121e7d71a1d5e91734a74cbfef5ae01f04
Marcio Bueno <mabuen@live.com>
2022-12-22 13:30:43 +0100

    borda e imagens dos produtos

--- projeto-site/index.php
+++ projeto-site/index.php
@@ -8,35 +8,44 @@
   <link rel="stylesheet" href="./src/index.css">
 </head>
 <body>
-  <div class="topo">
-    <div class="logo">
-       <p>MB<br>Soluções</p>
-    </div>
+  <div class="tronco">
+    <div class="topo">
+      <div class="logo">
+         <p>MB<br>Soluções</p>
+      </div>
     
-    <div class="login">
-      <form action="" class="formulario">
-        <input type="email" class="email" placeholder="seu e-mail">
-        <input type="password" name="" id="" class="senha" placeholder="sua senha">
-        <input type="submit" value="Logar" class="botao">
-      </form>
+      <div class="login">
+        <form action="" class="formulario">
+          <input type="email" class="email" placeholder="seu e-mail">
+          <input type="password" name="" id="" class="senha" placeholder="sua senha">
+          <input type="submit" value="Logar" class="botao">
+        </form>
+      </div>
     </div>
+      <div class="banner">
+        <!-- <img src="./src/banner.jpg" alt="banner distorcao gravitacional"> -->
+      </div>
+      <div class="menu">
+        para ferias
+      </div>
+      <div class="produtos">
+        <div class="p1">
+          para ferias produto título imagem e descricäo.
+          <img src="./src/website.png" alt="website">
+        </div>
 
-    <div class="banner">
-      <!-- <img src="./src/banner.jpg" alt="banner distorcao gravitacional"> -->
-    </div>
+        <div class="p2">
+          <img src="./src/network.png" alt="rede de computadores">
+        </div>
 
-    <div class="menu">
-      para ferias 
-    </div>
+        <div class="p3">
+          <img src="./src/seo.png" alt="otimização de mecanismos de busca">
+        </div>
 
-    <div class="p1">
-      para ferias produto título imagem e descricäo
-    </div>
-    <div class="p2"></div>
-    <div class="p3"></div>
-    <div class="p4"></div>
-
-
+        <div class="p4">
+          <img src="./src/social-media.png" alt="mídias sociais">
+        </div>
+      </div>
   </div>
 </body>
 </html>
